df490853fff6b75dbc83969d9430e4be8f182490
talas <talasbek.dadabaev@gmail.com>
2025-06-27 04:03:24 +0400

    changed pro-1

--- script.js
+++ script.js
@@ -8,46 +8,60 @@ const headerOpen =document.querySelector(".section-header");
     })
 
 // Scroll animations
-const homePage = document.querySelector(".container-general");
+const homePage = document.querySelector(".fade-in");
 const page = document.querySelector(".hero-img2");
 const firstSect = document.querySelector(".first-page");
 const advanteg = document.querySelector(".advanteg");
+const homeSect = document.querySelector(".container-general");
+const cards = document.querySelectorAll('.fade-up')
+
+function checkScrollBox () {
+  const bottom = window.innerHeight * 0.85;
+  cards.forEach(el => {
+    const cardTop = el.getBoundingClientRect().top;
+    if (cardTop < bottom) {
+        el.classList.add('show');
+    }
+  })
+}
 
 
  const scrollAnimation = () => {
     let windowCenter = window.scrollY;
-    let windowScroll = firstSect.offsetHeight / 2;
+    let windowScroll = firstSect.offsetHeight /2;
     if (windowCenter >= windowScroll) {
         page.classList.add('active');
         advanteg.classList.add('active');
-    } 
+    };
  }
 
 
-// Scroll header fixed
-const scrollFixed = () => {
+ window.addEventListener('DOMContentLoaded',()  => {
+    const elements = document.querySelectorAll('.fade-in');
+    elements.forEach((el, index) => {
+    setTimeout(()=> {
+    el.classList.add('active');
+ },index * 50 )
+    })
+ })
 
-   let centerY = window.scrollY;
-   let firstSect = homePage.offsetHeight / 10 ;
 
-    if (centerY >= firstSect ) {
-        headerOpen.classList.add("fixed");
-    }
-    else {
-        headerOpen.classList.remove("fixed");
-    }
+ 
 
-}
 
 window.addEventListener("scroll", () => {
-scrollFixed();
 scrollAnimation();
+checkScrollBox();
 })
 
-const sideBar = document.querySelector(".portfolio");
-let barOpen = document.querySelector('.aside-bar');
+window.addEventListener('load', checkScrollBox);
 
+const fastQa = document.querySelector(".add-inform");
+const textQa = document.querySelector(".text-scale");
 
-sideBar.addEventListener('click', () => {
-    barOpen.classList.toggle("side-open");
+fastQa.addEventListener('click', () => {
+    textQa.classList.toggle('text-scaleActive')
 })
+
+
+
